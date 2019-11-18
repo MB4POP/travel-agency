017e0ebd3f05c032165dd3940885aad530dcd429
Mariusz Białecki <mariusz.bialecki@onet.pl>
2019-11-18 14:36:48 +0100

    add start project

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, addTags, removeTags, changeDuration} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters
+  addTags: tag => dispatch(addTags(tag)),
+  removeTags: tag => dispatch(removeTags(tag)),
+  changeDuration: (frame, value) => dispatch(changeDuration(frame, value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
