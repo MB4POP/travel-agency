41d01be0c653153127c1ea5707da95248e38860a
Mariusz Białecki <mariusz.bialecki@onet.pl>
2019-11-25 17:50:30 +0100

    add OrderForm and OrderSummary

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -3,15 +3,16 @@ import React from 'react';
 import styles from './OrderSummary.scss';
 import PropTypes from 'prop-types';
 import {calculateTotal} from '../../../utils/calculateTotal.js';
-import formatPrice from '../../../utils/formatPrice.js';
+import {formatPrice} from '../../../utils/formatPrice.js';
 
 const OrderSummary = props => {
-  formatPrice(props.tripCost);
-  calculateTotal(formatPrice, props.options);
-
-  <h2 className={styles.component}>
-    Total<strong>{calculateTotal}</strong>
-  </h2>;
+  const fullPrice = calculateTotal(props.tripCost, props.options);
+  const priceFormat = formatPrice(fullPrice);
+  return (
+    <h2 className={styles.component}>
+      Total<strong>{priceFormat}</strong>
+    </h2>
+  );
 };
 
 OrderSummary.propTypes = {
